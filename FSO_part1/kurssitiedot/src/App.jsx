643fa228f10ed7429eed4a25a19977f4333ea1be
Max Perälä <max.perala@tuni.fi>
2024-09-09 20:06:42 +0300

    Tehtävät 1.5 asti

--- FSO_part1/kurssitiedot/src/App.jsx
+++ FSO_part1/kurssitiedot/src/App.jsx
@@ -1,17 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content courses={[{name: part1, exercises: exercises1}, {name: part2, exercises: exercises2}, {name: part3, exercises: exercises3}]} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content courses={course.parts} />
+      <Total courses={course.parts} />
     </div>
   )
 }
@@ -38,7 +48,11 @@ const Part = ({name, exercises}) => {
       )
 }
 
-const Total = ({total}) => {
+const Total = ({courses}) => {
+  let total = 0;
+  for (const c of courses) {
+    total += c.exercises
+  }
   return (
       <p>number of exercises {total}</p>
   )
