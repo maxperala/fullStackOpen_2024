4cc49cdaf98557bc764a9d30b41b7890ba3939e0
maxperala <max.perala@hotmail.fi>
2024-11-12 12:47:51 +0200

    Tehtävät 9.9 asti

---
+++ FSO_part9/patientor-backend/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import * as cors from "cors";
+
+const app = express();
+app.use(cors.default());
+app.get("/api/ping", (_, res) => {
+  console.log("ping recieved");
+  res.send("pong");
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
